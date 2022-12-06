5e1d605d3ca6c4145c7496697b081303604f3ab5
harisalkarni <haris.alkarni@gmail.com>
2022-12-06 13:16:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {useCallback, useEffect, useState} from 'react'
+
+
+
 
 function App() {
+  const [second, setSecond] = useState(0);
+  const [play, setPlay] = useState(false);
+
+
+  function formatTime(secon) {
+    const minute = Math.floor(secon / 60);
+    const second = secon % 60;
+    const formattingTime = ('0' + minute).slice(-2) + ":" + ('0' + second).slice(-2);
+  
+    return formattingTime;
+  }
+  
+
+  useEffect(() => {
+
+    if(!play) return;
+
+    const interval = setInterval(() => {
+      setSecond((previousSecond) => previousSecond + 1)
+    }, 1000)
+    
+    return () => {
+      clearInterval(interval);
+    }
+  }, [play])
+
+  const onReset = useCallback(() => {
+    setSecond(0)
+  }, []);
+  
+  const onToogle = useCallback(() => {
+    setPlay((previousPlay) => !previousPlay)
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{formatTime(second)}</h1>
+      <button onClick={onToogle}>{!play ? "Start" : "Stop"}</button>
+      <button onClick={onReset}>Reset</button>
     </div>
   );
 }
